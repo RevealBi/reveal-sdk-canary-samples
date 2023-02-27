5ed06260eef07a591da58dc76d7600f12d992fa4
Brian Lagunas <brianlagunas@users.noreply.github.com>
2023-02-27 15:18:37 -0700

    added react app

---
+++ react/src/App.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react';
+import { DashboardViewer, RevealSdkSettings } from 'reveal-sdk-canary';
+import './App.css';
+
+RevealSdkSettings.ServerUrl = "https://samples.revealbi.io/upmedia-backend/reveal-api/";
+
+function App() {
+  
+  useEffect(() => {
+    var dv = new DashboardViewer("#revealView", "Sales", {
+      dataSources: [
+        { type: "REST", title: "Sales by Category", subtitle: "Excel2Json", url: "https://excel2json.io/api/share/6e0f06b3-72d3-4fec-7984-08da43f56bb9" },
+        {
+            type: "MicrosoftSqlServer", id: "MySQLServer", title: "My SQL Server", items: [
+                { table: "Products", title: "My Products Table" },
+                { table: "Orders", title: "My Orders Table" },
+                { table: "OrderDetails", title: "My OrderDetails Table" },
+            ]
+        },
+      ]
+    });
+  }, [])
+
+  return (
+    <div id="revealView" style={{height: "100vh", width: "100%"}}></div>
+  );
+}
+
+export default App;
