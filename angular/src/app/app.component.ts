549e6490fee8301f40eb31da8c182ff7aa1eb353
Brian Lagunas <835562+brianlagunas@users.noreply.github.com>
2023-10-26 17:41:00 -0600

    updated angular sample

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { RevealSdkSettings, DashboardViewerOptions, MenuItem } from '@revealbi/ui';
+import { AfterViewInit, Component } from '@angular/core';
+import { DashboardDateFilter, DashboardLink, DateField, DateLinkFilter, FunnelChartVisualization, LinkFilter, NumberField, PivotVisualization, RdashDocument, RestServiceBuilder, TextField, UrlLink, VisualizationLinker } from '@revealbi/dom';
+import { RevealSdkSettings, DashboardViewerOptions, MenuItem, SeriesColorRequestedArgs } from '@revealbi/ui';
 
 RevealSdkSettings.serverUrl = "https://samples.revealbi.io/upmedia-backend/reveal-api/";
 
@@ -10,7 +11,7 @@ RevealSdkSettings.serverUrl = "https://samples.revealbi.io/upmedia-backend/revea
 })
 export class AppComponent {
   title = 'RevealUIAngularDemo';
-  dashboard: string = "Sales";
+  dashboard: any;
   options: DashboardViewerOptions = {
     filters: {
       showFilters: false,
@@ -26,4 +27,63 @@ export class AppComponent {
       }
     },
   }  
+
+  constructor() {
+    this.generateDashboard().then(dashboard => {
+      this.dashboard = dashboard;
+    });
+  }
+
+  async generateDashboard() {
+    const excelDataSourceItem = new RestServiceBuilder("http://dl.infragistics.com/reportplus/reveal/samples/Samples.xlsx")
+    .setTitle("Excel Data Source")
+    .setSubtitle("Marketing Sheet")
+    .useExcel("Marketing")
+    .setFields([
+        new DateField("Date"),
+        new NumberField("Spend"),
+        new NumberField("Budget"),
+        new NumberField("CTR"),
+        new NumberField("Avg. CPC"),
+        new NumberField("Traffic"),
+        new NumberField("Paid Traffic"),
+        new NumberField("Other Traffic"),
+        new NumberField("Conversions"),
+        new TextField("Territory"),
+        new TextField("CampaignID"),
+        new NumberField("New Seats"),
+        new NumberField("Paid %"),
+        new NumberField("Organic %")
+    ])
+    .build();
+
+    const document = new RdashDocument("Linking Dashboard");
+        
+    document.filters.push(new DashboardDateFilter());
+
+    const funnel = new FunnelChartVisualization("Funnel Chart", excelDataSourceItem).setLabel("CampaignID").setValue("Conversions");
+
+    const linkedDocument = await RdashDocument.load("Campaigns");
+    const filter = linkedDocument.filters.find(f => f.title === "CampaignID")!;
+
+    const linker = new VisualizationLinker();
+    linker.links = [
+        new UrlLink("Open URL", "https://www.brianlagunas.com/[CampaignID]"),
+        new DashboardLink("Open Dashboard", "Campaigns", 
+            [ 
+                new LinkFilter("Campaigns Filter", filter.id, filter.title),  
+                new DateLinkFilter() 
+            ])
+    ]
+    funnel.linker = linker;
+
+    const pivot = new PivotVisualization("New Seats by Campaign ID", excelDataSourceItem).setRow("CampaignID").setValues("CTR", "Avg. CPC", "New Seats");
+    pivot.linker = new VisualizationLinker()
+        .addUrl("Open URL", "https://www.brianlagunas.com/[CampaignID]")
+        .addDashboard("Open Dashboard", "Campaigns", [ new LinkFilter("Campaigns Filter", filter.id, filter.title),  new DateLinkFilter() ]);
+    
+    document.visualizations = [funnel, pivot];
+
+    return document;
+  }
 }
